bf5ad5a6f2fa74e4bf6f67107921d0747d409317
dlr514 <62752815+dlr514@users.noreply.github.com>
2021-12-14 09:40:37 +0100

    Add files via upload

---
+++ socrates.py
@@ -0,0 +1,19 @@
+#1 Socrates is human
+
+Human = "mortal"
+Socrates = Human
+print(Socrates)
+
+#2 logical validity a condition is true only if its prequisities are true
+
+Statementa = True
+Statementb = True
+if Statementa and Statementb == True:
+	condition = True
+	print(condition)
+	k=input("press any key to exit") 
+
+else: 
+	print("wrong")
+	k=input("press any key to exit") 
+	
